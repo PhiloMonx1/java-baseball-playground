7cb19da271711053ff94d7575b83968800f46f0a
EH13\philo <philomonx1@gmail.com>
2024-02-22 18:31:17 +0900

    Feat : StringTest 요구사항 3번 *test : getCharAtPosition() test case 추가 *test : getCharAtPositionIndexOutOfBoundsException() exception case 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -43,4 +44,25 @@ public class StringTest {
         assertThat(actual).isEqualTo("1,2");
     }
 
+
+    //요구사항 3
+    @Test
+    @DisplayName("특정 위치의 문자 가져오기")
+    void getCharAtPosition(){
+        String input = "abc";
+        char actual = input.charAt(1);
+
+        assertThat(actual).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("특정 위치의 문자 가져오기 (예외 발생 : IndexOutOfBounds)")
+    void getCharAtPositionIndexOutOfBoundsException() {
+        String input = "abc";
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+            .isThrownBy(() -> {
+                char actual = input.charAt(55);
+            }).withMessageMatching("String index out of range: 55");
+    }
 }
