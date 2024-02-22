e2ad68508e14e5ba2dcf3ca5bd16d9c4e44e4c6f
LEE TAE MIN <philomonx1@gmail.com>
2024-02-23 01:48:30 +0900

    Feat : 문자열 계산기 calculatorNotPemdas() 추가 ( PEMDAS 규칙을 무시하는 계산기 ) *add : StringCalculator 클래스 추가 *add : Operator Enum 추가 *chore : 자바 JDK 버전 변경 ( 1.8 -> 14 )

---
+++ src/main/java/study/StringCalculator.java
@@ -0,0 +1,38 @@
+package study;
+
+import study.constants.Operator;
+
+import java.util.*;
+
+public class StringCalculator {
+	private Scanner scanner = new Scanner(System.in);
+
+	public int calculatorNotPemdas(){
+		String value = scanner.nextLine();
+		int result = 0;
+		Operator operator = Operator.ADD;
+		String[] values = value.split(" ");
+
+		for (String var : values){
+			switch (var){
+				case "+" -> operator = Operator.ADD;
+				case "-" -> operator = Operator.SUBTRACT;
+				case "*" -> operator = Operator.MULTIPLY;
+				case "/" -> operator = Operator.DIVIDE;
+				default -> {
+					switch (operator){
+						case SUBTRACT -> result -= Integer.parseInt(var);
+						case MULTIPLY -> result *= Integer.parseInt(var);
+						case DIVIDE   -> result /= Integer.parseInt(var);
+						default       -> result += Integer.parseInt(var);
+					}
+					operator = Operator.ADD;
+				}
+			}
+		}
+
+		scanner.close();
+		return result;
+	}
+
+}
