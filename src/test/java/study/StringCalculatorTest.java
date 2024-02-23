43fadea406014cd172c046152c33ee9d21977afa
LEE TAE MIN <philomonx1@gmail.com>
2024-02-23 12:43:32 +0900

    Test : 	estCalculatorNotPemdas() 테스트 코드 추가

--- src/test/java/study/StringCalculatorTest.java
+++ src/test/java/study/StringCalculatorTest.java
@@ -1,2 +1,21 @@
-package study;public class StringCalculatorTest {
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+	protected void systemIn(String input) {
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+	}
+
+	@Test
+	void testCalculatorNotPemdas(){
+		systemIn("2 + 3 * 4 / 2");
+		StringCalculator stringCalculator = new StringCalculator();
+
+		assertEquals(10 , stringCalculator.calculatorNotPemdas());
+	}
 }
