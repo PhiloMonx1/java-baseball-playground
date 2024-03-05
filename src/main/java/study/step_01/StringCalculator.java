73df69a2a6e8659230f7b21a9db252805c114d68
LEE TAE MIN <philomonx1@gmail.com>
2024-03-05 16:21:44 +0900

    Refactor : `Operator` Enum 대신 상수로 사용하도록 수정

--- src/main/java/study/step_01/StringCalculator.java
+++ src/main/java/study/step_01/StringCalculator.java
@@ -1,34 +1,37 @@
 package study.step_01;
 
-import study.step_01.constants.Operator;
-
 import java.util.Scanner;
 
 public class StringCalculator {
+	private final char PLUS = '+';
+	private final char MINUS = '-';
+	private final char MULTIPLY = '*';
+	private final char DIVIDE = '/';
+
 
 	public int calculatorNotPemdas(){
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
 
 		int result = 0;
-		Operator operator = Operator.ADD;
+		char operator = '+';
 
 		String[] values = input.split(" ");
 
 		for (String value : values){
 			switch (value){
-				case "+" -> operator = Operator.ADD;
-				case "-" -> operator = Operator.SUBTRACT;
-				case "*" -> operator = Operator.MULTIPLY;
-				case "/" -> operator = Operator.DIVIDE;
+				case "+" -> operator = PLUS;
+				case "-" -> operator = MINUS;
+				case "*" -> operator = MULTIPLY;
+				case "/" -> operator = DIVIDE;
 				default -> {
 					switch (operator){
-						case SUBTRACT -> result -= Integer.parseInt(value);
+						case MINUS    -> result -= Integer.parseInt(value);
 						case MULTIPLY -> result *= Integer.parseInt(value);
 						case DIVIDE   -> result /= Integer.parseInt(value);
 						default       -> result += Integer.parseInt(value);
 					}
-					operator = Operator.ADD;
+					operator = PLUS;
 				}
 			}
 		}
