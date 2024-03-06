31bec3aa16feb51ecc722045282b68940000d3c1
LEE TAE MIN <philomonx1@gmail.com>
2024-03-06 17:15:55 +0900

    Comment : `StringCalculator` 클래스 내부 함수 주석 추가

--- src/main/java/study/step_01/StringCalculator.java
+++ src/main/java/study/step_01/StringCalculator.java
@@ -31,6 +31,12 @@ public class StringCalculator {
 		return result;
 	}
 
+	/**
+	 * String 형태의 연산자를 char 형태로 parsing 하는 함수
+	 *
+	 * @param value "+", "-", "*", "/"
+	 * @return char PLUS, MINUS, MULTIPLY, DIVIDE
+	 */
 	public char getOperator(String value) {
 		char operator;
 		switch (value) {
@@ -43,6 +49,16 @@ public class StringCalculator {
 		return operator;
 	}
 
+	/**
+	 * &#064;Override
+	 * <p>
+	 * String 형태의 연산자를 char 형태로 parsing 하는 함수,
+	 * 기존에 주어진 char operator가 존재할 경우 String value에 잘못된 값이 들어와도 두 번째 인자의 operator를 반환.
+	 *
+	 * @param value "+", "-", "*", "/"
+	 * @param operator  PLUS, MINUS, MULTIPLY, DIVIDE
+	 * @return char PLUS, MINUS, MULTIPLY, DIVIDE
+	 */
 	public char getOperator(String value, char operator) {
 		if( !OPERATOR_PATTERN.matcher(value).matches() ){
 			return operator;
@@ -50,6 +66,14 @@ public class StringCalculator {
 		return getOperator(value);
 	}
 
+	/**
+	 * 첫 번째 인자와 세 번째 인자를 두 번째 인자로 사칙연산하는 함수
+	 *
+	 * @param result 왼쪽 피연산자
+	 * @param operator PLUS, MINUS, MULTIPLY, DIVIDE
+	 * @param operand 오른쪽 피연산자
+	 * @return result operator operand = result
+	 */
 	public int applyOperation(int result, char operator, int operand) {
 		switch (operator) {
 			case MINUS    -> result -= operand;
@@ -60,6 +84,17 @@ public class StringCalculator {
 		return result;
 	}
 
+	/**
+	 * &#064;Override
+	 * <p>
+	 * 첫 번째 인자(int)와 세 번째 인자(String)를 두 번째 인자로 사칙연산하는 함수,
+	 * 이 때 세 번째 인자는 parseInt()처리 후 계산
+	 *
+	 * @param result 왼쪽 피연산자
+	 * @param operator PLUS, MINUS, MULTIPLY, DIVIDE
+	 * @param value 오른쪽 피연산자 (parseInt()처리 후 계산)
+	 * @return result operator operand = result
+	 */
 	public int applyOperation(int result, char operator, String value){
 		if(!NUMBER_PATTERN.matcher(value).matches()){
 			return result;
