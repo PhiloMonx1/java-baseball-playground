95f5dfa268a05cf26bf82bfd2d5a9c78b3a032d5
LEE TAE MIN <philomonx1@gmail.com>
2024-03-06 09:40:39 +0900

    Refactor : `calculatorNotPemdas()` 함수 리네임 & 함수 로직 분리 *refactor : Scanner 사용부 Try With Resources 처리 *refactor : 클래스 내부 상수 static 처리 *test : `StringCalculator` 클래스 내부 함수 test 코드 작성

--- src/main/java/study/step_01/StringCalculator.java
+++ src/main/java/study/step_01/StringCalculator.java
@@ -3,20 +3,23 @@ package study.step_01;
 import java.util.Scanner;
 
 public class StringCalculator {
-	private final char PLUS = '+';
-	private final char MINUS = '-';
-	private final char MULTIPLY = '*';
-	private final char DIVIDE = '/';
+	private static final char PLUS = '+';
+	private static final char MINUS = '-';
+	private static final char MULTIPLY = '*';
+	private static final char DIVIDE = '/';
 
 
-	public int calculatorNotPemdas(){
-		Scanner scanner = new Scanner(System.in);
-		String input = scanner.nextLine();
+	public int getInputAndCalculate(){
+		try (Scanner scanner = new Scanner(System.in)) {
+			String input = scanner.nextLine();
+			String[] values = input.split(" ");
+			return calculatorNotPemdas(values);
+		}
+	}
 
+	public int calculatorNotPemdas(String[] values){
 		int result = 0;
 		char operator = '+';
-
-		String[] values = input.split(" ");
 
 		for (String value : values){
 			switch (value){
@@ -25,18 +28,23 @@ public class StringCalculator {
 				case "*" -> operator = MULTIPLY;
 				case "/" -> operator = DIVIDE;
 				default -> {
-					switch (operator){
-						case MINUS    -> result -= Integer.parseInt(value);
-						case MULTIPLY -> result *= Integer.parseInt(value);
-						case DIVIDE   -> result /= Integer.parseInt(value);
-						default       -> result += Integer.parseInt(value);
-					}
+					int operand = Integer.parseInt(value);
+					result = applyOperation(result, operator, operand);
 					operator = PLUS;
 				}
 			}
 		}
 
-		scanner.close();
+		return result;
+	}
+
+	public int applyOperation(int result, char operator, int operand) {
+		switch (operator) {
+			case MINUS    -> result -= operand;
+			case MULTIPLY -> result *= operand;
+			case DIVIDE   -> result /= operand;
+			default       -> result += operand;
+		}
 		return result;
 	}
 
--- src/test/java/study/step_01/StringCalculatorTest.java
+++ src/test/java/study/step_01/StringCalculatorTest.java
@@ -1,21 +1,40 @@
 package study.step_01;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
+	private static final StringCalculator stringCalculator = new StringCalculator();
+
 	protected void systemIn(String input) {
 		System.setIn(new ByteArrayInputStream(input.getBytes()));
 	}
 
 	@Test
-	void testCalculatorNotPemdas(){
+	@DisplayName("systemIn 인풋 값을 Pemdas를 무시하고 순서대로 사칙연산")
+	void testGetInputAndCalculate() {
 		systemIn("2 + 3 * 4 / 2");
-		StringCalculator stringCalculator = new StringCalculator();
 
-		assertEquals(10 , stringCalculator.calculatorNotPemdas());
+		assertEquals(10, stringCalculator.getInputAndCalculate());
+	}
+
+	@Test
+	@DisplayName("첫 번째 인자와 세번 째 인자를 가운데 인자의 연산자로 계산")
+	void applyOperation() {
+		int actual = stringCalculator.applyOperation(2, '*', 3);
+		assertThat(actual).isEqualTo(6);
+	}
+
+	@Test
+	@DisplayName("String[]의 식을 Pemdas를 무시하고 순서대로 사칙연산")
+	void calculatorNotPemdas() {
+		String[] values = {"2", "+", "3", "*", "4", "/", "2"};
+		int actual = stringCalculator.calculatorNotPemdas(values);
+		assertThat(actual).isEqualTo(10);
 	}
 }
