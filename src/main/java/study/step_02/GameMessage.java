02eed0d40758d7f3bad301145c869916aed40e5b
LEE TAE MIN <philomonx1@gmail.com>
2024-03-07 17:40:25 +0900

    Feat : 게임 메시지, 예외 메시지 final String으로 선언 *add : `GameMessage.java` 클래스 추가 (게임 메시지 관련 클래스) *add : `GameMessage.java` 클래스 메시지 print 함수 추가 *add : `ExceptionMessage.java` 클래스 추가 (예외 메시지 관련 클래스)

---
+++ src/main/java/study/step_02/GameMessage.java
@@ -0,0 +1,39 @@
+package study.step_02;
+
+public class GameMessage {
+	public static final String SWING = "숫자를 입력해 주세요 : ";
+	public static final String STRIKE = "스트라이크";
+	public static final String BALL = "볼";
+	public static final String NOTING = "낫싱";
+	public static final String WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	public static final String REPLAY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+	public static void displaySwingMessage() {
+		System.out.println(SWING);
+	}
+
+	public static void displayStrikeMessage(int count) {
+		if(count > 0) {
+			System.out.println(count + STRIKE);
+		}
+	}
+
+	public static void displayBallMessage(int count) {
+		if(count > 0) {
+			System.out.println(count + BALL);
+		}
+	}
+
+	public static void displayNotingMessage() {
+		System.out.println(NOTING);
+	}
+
+	public static void displayWinMessage() {
+		System.out.println(WIN);
+	}
+
+	public static void displayReplayMessage() {
+		System.out.println(REPLAY);
+	}
+
+}
